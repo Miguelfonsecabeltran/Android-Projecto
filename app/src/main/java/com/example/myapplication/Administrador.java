a61f63f823f6a7f5576d9e7ec8973512f0f3df4a
Manuel9490 <45578962+Manuel9490@users.noreply.github.com>
2020-11-20 00:29:51 -0500

    Update Administrador.java

--- app/src/main/java/com/example/myapplication/Administrador.java
+++ app/src/main/java/com/example/myapplication/Administrador.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import com.example.myapplication.Usuario;
+
 public class  Administrador extends Usuario {
     public Administrador(String correo, String nombre, String password, String rol) {
         super(correo, nombre, password, rol);
